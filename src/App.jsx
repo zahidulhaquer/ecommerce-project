e3b8dd14591384fc6e7260890ea2cb11f8b5558b
Zahidul Haque <zahidulhaquer@gmail.com>
2025-09-01 17:34:19 +0600

    Initial commit - Ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
+import './App.css'
+
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+  )
+}
+
+export default App
+ 
