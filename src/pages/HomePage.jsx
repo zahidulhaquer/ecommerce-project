f9cf31937c3057e5751db3cdbe0d08117932a53d
Zahidul Haque <zahidulhaquer@gmail.com>
2025-09-02 05:03:35 +0600

    Fetch data from the beckend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,19 @@ import { products } from '../../starting-code/data/products';
 import "./HomePage.css";
 
 export function HomePage() {
+  fetch(`http://localhost:3000/api/products`)
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+    })
+
   return (
     <>
       <title>Ecommerce Project</title>
 
       <Header />
-      
+
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => {
