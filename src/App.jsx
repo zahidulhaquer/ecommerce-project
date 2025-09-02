0e5bfea35498111a90e3e161f4d4271fff51ea77
Zahidul Haque <zahidulhaquer@gmail.com>
2025-09-02 18:59:50 +0600

    Lift cart up and share it between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { Routes, Route } from 'react-router'
+import { useState, useEffect } from 'react'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
@@ -7,10 +9,19 @@ import './App.css'
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/cart-items`)
+      .then((response) => {
+        setCart(response.data);
+      })
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
@@ -18,4 +29,3 @@ function App() {
 }
 
 export default App
- 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,8 @@ import { Header } from '../components/header';
 // import { products } from '../../starting-code/data/products';
 import "./HomePage.css";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get(`/api/products`)
@@ -14,10 +13,6 @@ export function HomePage() {
         setProducts(response.data);
       });
 
-      axios.get(`/api/cart-items`)
-        .then((response) => {
-          setCart(response.data);
-        })
   }, [])
 
   return (
