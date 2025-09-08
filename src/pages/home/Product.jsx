13fd9b18072950262b495ac6393f2a849d3bcbf3
Zahidul Haque <zahidulhaquer@gmail.com>
2025-09-09 03:24:31 +0600

    Test clicking add to cart button

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -67,6 +67,7 @@ export function Product({ product, loadCart }) {
             </div>
 
             <button className="add-to-cart-button button-primary"
+            data-testid ="add-to-cart-button"
                 onClick={addToCart}>
                 Add to Cart
             </button>
--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { it, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import { Product } from './Product';
+
+vi.mock('axios');
 
 describe('Product component', () => {
     it('display the product details correctly', () => {
@@ -40,4 +44,35 @@ describe('Product component', () => {
             screen.getByText('87')
         ).toBeInTheDocument();
     });
+
+    it('add a product to the cart', async () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+
+        const loadCart = vi.fn();
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        );
+        expect(loadCart).toHaveBeenCalled();
+    })
 });
